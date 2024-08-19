5dd353456e2a8ef61beecf9a225ccf3b898c2951
Drew Alexander <drewalexander986@gmail.com>
2024-08-19 16:22:29 +0800

    spi mode

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ use esp_backtrace as _;
 use esp_hal::{
     clock::ClockControl,
     delay::Delay,
-    gpio::{Io, Level, Output},
-    peripherals::Peripherals,
+    gpio::{self, Io, Level, Output},
+    peripherals::{Peripherals, SPI2},
     prelude::*,
-    system::SystemControl,
+    spi::{master::Spi, FullDuplexMode, SpiMode},
+    system::SystemControl
 };
 
 #[entry]
@@ -20,22 +21,51 @@ fn main() -> ! {
     let delay = Delay::new(&clocks);
     
     esp_println::logger::init_logger_from_env();
-    log::info!("Hello world!");
 
     let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
-    let mut lak = Output::new(io.pins.gpio10, Level::Low);
-    let mut clk = Output::new(io.pins.gpio12, Level::Low);
-    let mut da = Output::new(io.pins.gpio11, Level::Low);
-    let mut en = Output::new(io.pins.gpio13, Level::Low);
+    let mut lak = Output::new(io.pins.gpio1, Level::Low);
+    // let mut clk = Output::new(io.pins.gpio12, Level::Low);
+    // let mut da = Output::new(io.pins.gpio11, Level::Low);
+    let mut en = Output::new(io.pins.gpio4, Level::Low);
 
+    let clk = io.pins.gpio2;
+    let da = io.pins.gpio3;
+
+    let mut spi = Spi::new(
+        peripherals.SPI2,
+        40u32.MHz(),
+        SpiMode::Mode0,
+        &clocks,
+    ).with_pins(Some(clk), Some(da), gpio::NO_PIN, gpio::NO_PIN);
+    log::info!("HELLO");
     lak.set_high();
-    da.set_high();
     en.set_low();
-    clk.set_low();
-
     loop {
-        clk.set_high();
-        clk.set_low();
-        delay.delay(100.millis());
+        for i in 0..1000 {
+            set_brightness(i, &mut spi);
+        }
+        for i in (0..1000).rev() {
+            set_brightness(i, &mut spi);
+        }
+    }
+
+}
+
+fn set_brightness(value: u32, spi: &mut Spi<SPI2, FullDuplexMode>) {
+    let mut on_data: [u8; 32] = [0xff; 32];
+    let mut off_data: [u8; 32] = [0x00; 32];
+
+    const DELAY_TIME: u32 = 1;
+    const MAX_VALUE: u32 = 500;
+    let ratio: f32 = value as f32 / 1000.0;
+    let norm_val = (ratio * MAX_VALUE as f32) as u32;
+
+    for _ in 0..DELAY_TIME {
+        for _ in 0..norm_val {
+            let _ = Spi::write_bytes(spi, &mut on_data);
+        }
+        for _ in 0..MAX_VALUE-norm_val {
+            let _ = Spi::write_bytes(spi, &mut off_data);
+        }
     }
 }
