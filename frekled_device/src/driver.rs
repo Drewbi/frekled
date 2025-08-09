0a2089e403c8f146216e68fc426da455dc34ba28
Drew Alexander <drewalexander986@gmail.com>
2025-08-09 18:41:02 +0800

    WIP debug implenetation

--- frekled_device/src/driver.rs
+++ frekled_device/src/driver.rs
@@ -20,7 +20,7 @@ static DEVICE: Mutex<RefCell<Option<Device>>> = Mutex::new(RefCell::new(None));
 
 static DISPLAY_BUFFER: Mutex<RefCell<Option<[Timeslice; NUM_SLICES]>>> = Mutex::new(RefCell::new(None));
 static UPDATE_BUFFER: Mutex<RefCell<Option<[Timeslice; NUM_SLICES]>>> = Mutex::new(RefCell::new(None));
-static SWAP_BUFFERS: Mutex<RefCell<bool>> = Mutex::new(RefCell::new(false));
+static SWAP_FLAG: Mutex<RefCell<bool>> = Mutex::new(RefCell::new(false));
 static DISPLAY_POSITION: Mutex<RefCell<u32>> = Mutex::new(RefCell::new(0));
 
 pub struct Device<'a> {
@@ -76,8 +76,8 @@ pub fn update(frame: &Frame) {
 
     critical_section::with(|cs| {
         UPDATE_BUFFER.borrow_ref_mut(cs).replace(packets);
-        let mut swap_buffers = SWAP_BUFFERS.borrow_ref_mut(cs);
-        *swap_buffers = true;
+        let mut swap_flag = SWAP_FLAG.borrow_ref_mut(cs);
+        *swap_flag = true;
     })
 }
 
@@ -89,15 +89,14 @@ fn transmit(packet: Timeslice, device: &mut Device) {
 
 #[handler]
 fn display_interrupt() {
-    // let start_time = time::current_time();
     critical_section::with(|cs| {
         let mut display_buffer_binding = DISPLAY_BUFFER.borrow_ref_mut(cs);
 
-        let mut swap_buffers = SWAP_BUFFERS.borrow_ref_mut(cs);
+        let mut swap_flag = SWAP_FLAG.borrow_ref_mut(cs);
         let mut display_position = DISPLAY_POSITION.borrow_ref_mut(cs);
 
-        if *swap_buffers {
-            *swap_buffers = false;
+        if *swap_flag {
+            *swap_flag = false;
             *display_position = 0;
 
             let mut update_buffer = UPDATE_BUFFER.borrow_ref_mut(cs);
@@ -119,9 +118,7 @@ fn display_interrupt() {
 
         let position_exponent = (2 as u64).pow(*display_position);
         let delay_time = INTERRUPT_DELAY * position_exponent;
-        // log::info!("--------");
-        // log::info!("{}", *display_position);
-        // log::info!("{}", delay_time);
+        
         *display_position = (1 + *display_position) & PixelBitDepth::BITS - 1 as u32;
 
         timer0.clear_interrupt();
@@ -129,6 +126,4 @@ fn display_interrupt() {
         timer0.start();
 
     });
-    // let end_time = time::current_time();
-    // log::info!("Finish interrupt - {}", end_time - start_time);
 }
--- frekled_util/src/lib.rs
+++ frekled_util/src/lib.rs
@@ -2,33 +2,43 @@
 
 use bcm::calc_bcm_value;
 
-const CHUNK_SIZE: usize = u8::BITS as usize;
-const NUM_CHUNKS: usize = NUM_PIXELS / CHUNK_SIZE;
-pub type Timeslice = [u8; NUM_CHUNKS];
-
 pub type PixelBitDepth = u8;
 pub const NUM_SLICES: usize = PixelBitDepth::BITS as usize;
 pub const NUM_PIXELS: usize = 256;
-pub type Frame = [f64; NUM_PIXELS];
+
+const CHUNK_SIZE: usize = u8::BITS as usize;
+const NUM_CHUNKS: usize = NUM_PIXELS / CHUNK_SIZE;
+
+
+// Input Frame, one brightness value (0.0 - 1.0) per pixel
+pub type Frame = [f64; NUM_PIXELS]; 
+// Partial binary payload, displayed with correct delay to drive brightness
+pub type Timeslice = [u8; NUM_CHUNKS]; 
 
 mod bcm;
 
+// pub fn encode_packets(frame: &Frame, slices: &mut [Timeslice; NUM_SLICES]) {
+//     let mut out_value: u8;
+//     let mut bit_index: u8;
+
+//     for timeslice_index in 0..u8::BITS {
+//         for chunk_index in 0..NUM_CHUNKS {
+//             out_value = 0;
+//             bit_index = 1;
+
+//             for pixel_index in 0..CHUNK_SIZE {
+//                 if calc_bcm_value(frame[chunk_index + pixel_index]) & (1 << timeslice_index) != 0 {
+//                     out_value |= bit_index;
+//                 }
+//                 bit_index <<= 1;
+//             }
+//             slices[timeslice_index as usize][chunk_index] = out_value;
+//         }
+//     }
+// }
+
 pub fn encode_packets(frame: &Frame, slices: &mut [Timeslice; NUM_SLICES]) {
-    let mut out_value: u8;
-    let mut bit_index: u8;
-
-    for timeslice_index in 0..u8::BITS {
-        for chunk_index in 0..NUM_CHUNKS {
-            out_value = 0;
-            bit_index = 1;
-
-            for pixel_index in 0..CHUNK_SIZE {
-                if calc_bcm_value(frame[chunk_index + pixel_index]) & (1 << timeslice_index) != 0 {
-                    out_value |= bit_index;
-                }
-                bit_index <<= 1;
-            }
-            slices[timeslice_index as usize][chunk_index] = out_value;
-        }
+    for i in 0..256 {
+        slices[i] = []
     }
 }
